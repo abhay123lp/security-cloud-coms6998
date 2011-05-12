ce8fa5fa8ace8af464818d3559ec59936b142ecd
jaya.allamsetty <jaya.allamsetty@gmail.com>
2011-05-12 06:57:58 +0000

    Addded more functionality for groups

--- SecurityCloud/src/coms6998/security/SecuredCloudManager.java
+++ SecurityCloud/src/coms6998/security/SecuredCloudManager.java
@@ -13,41 +13,46 @@ import coms6998.security.FileObject.FilePermission;
  */
 public class SecuredCloudManager {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//final String BUCKET_NAME = "edu.columbia.cloud.test";
+		
+		// create a single bucket for all the users		
+		S3 s3 = S3.getInstance();
+		S3Bucket bucket = s3.createBucket("jla2164");
+		
+		// create 3 users
+		User user1 = User.getInstance("user1", "password");
+		User user2 = User.getInstance("user2", "password");
+		User user3 = User.getInstance("user3", "password");
+		
+		// create 3 groups
+		
 
-        final String BUCKET_NAME = "edu.columbia.cloud.test";
-
-        String username = "user";
-        String password = "passwordpassword";
-
-        // create a single bucket for all the users		
-        S3 s3 = S3.getInstance();
-        S3Bucket bucket = s3.createBucket("jla2164");
-
-        // create a user
-        User user = User.getInstance(username, password);
-
-        // create a file object and set permissions on it
-        FileObject file = new FileObject("TestFile", "this is sample file");
-        file.setPermission(FilePermission.Private);
-
-        try {
-            user.uploadFile(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            user.downloadFile("TestFile");
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
+		
+		
+		
+		// create a file object and set permissions on it
+		FileObject file = new FileObject("TestFile", "this is a sample file");
+		file.setPermission(FilePermission.Private);
+		
+		try {
+			user1.uploadFile(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			user1.downloadFile("TestFile");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
 
 }
