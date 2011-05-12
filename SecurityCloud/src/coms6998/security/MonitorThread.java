aaa700d81e18539ee51e39a51d57f849469a9e8e
mohankrishna10 <mohankrishna10@gmail.com>
2011-05-12 05:27:50 +0000

    Changing tabs to spaces, not a big fan of tabs

--- SecurityCloud/src/coms6998/security/MonitorThread.java
+++ SecurityCloud/src/coms6998/security/MonitorThread.java
@@ -5,105 +5,105 @@ import java.util.Calendar;
 
 public class MonitorThread implements Runnable {
 
-	public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
-	public static final String RESTART_TIME = " 22:";
-	private String username;
-	private String secGroupName;
-	private String keyPairName;
-	private String amiName;
+    public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+    public static final String RESTART_TIME = " 22:";
+    private String username;
+    private String secGroupName;
+    private String keyPairName;
+    private String amiName;
 
-	/**
-	 * This method retrieves the current system time in the format specified
-	 * 
-	 * @return current time
-	 */
-	public static String now() {
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-		return sdf.format(cal.getTime());
-	}
+    /**
+     * This method retrieves the current system time in the format specified
+     * 
+     * @return current time
+     */
+    public static String now() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
 
-	/**
-	 * Default constructor for the monitor thread
-	 * 
-	 * @param user
-	 *            - username used as key for creating/identifying a instance
-	 * @param group
-	 *            - name of the security group to be associated with the
-	 *            instance
-	 * @param key
-	 *            - name of the key pair to be associated with the instance
-	 * @param ami
-	 *            - name of the AMI snapshot to be created
-	 */
-	public MonitorThread(String user, String group, String key, String ami) {
-		this.username = user;
-		this.amiName = ami;
-		this.secGroupName = group;
-		this.keyPairName = key;
-	}
+    /**
+     * Default constructor for the monitor thread
+     * 
+     * @param user
+     *            - username used as key for creating/identifying a instance
+     * @param group
+     *            - name of the security group to be associated with the
+     *            instance
+     * @param key
+     *            - name of the key pair to be associated with the instance
+     * @param ami
+     *            - name of the AMI snapshot to be created
+     */
+    public MonitorThread(String user, String group, String key, String ami) {
+        this.username = user;
+        this.amiName = ami;
+        this.secGroupName = group;
+        this.keyPairName = key;
+    }
 
-	public void run() {
+    public void run() {
 
-		// provision the user for the first time
-		AwsProvision user1 = AwsProvision.getInstance(this.username,
-				this.keyPairName, this.secGroupName, this.amiName);
+        // provision the user for the first time
+        AwsProvision user1 = AwsProvision.getInstance(this.username,
+                this.keyPairName, this.secGroupName, this.amiName);
 
-		while (true) {
-			
-			// start monitoring the VM for usage
-			boolean state = true;
-			do {
-				// keep checking every 10 mins
-				try {
-					Thread.sleep(605000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+        while (true) {
 
-				// monitorVM returns false if cpu utilization
-				// is less than the threshold to break out of while
-				state = user1.monitorVM();
+            // start monitoring the VM for usage
+            boolean state = true;
+            do {
+                // keep checking every 10 mins
+                try {
+                    Thread.sleep(605000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-			} while (state);
+                // monitorVM returns false if cpu utilization
+                // is less than the threshold to break out of while
+                state = user1.monitorVM();
 
-			// shutdown the VM and bring it up the next morning
-			if (!state) {
+            } while (state);
 
-				System.out.println("Cpu Utilization less than threshold, " +
-						"shutting down VM !!");
+            // shutdown the VM and bring it up the next morning
+            if (!state) {
 
-				// detach volume and create a snapshot before 
-				// shutting down the VM
-				user1.shutdownVM();
+                System.out.println("Cpu Utilization less than threshold, " +
+                "shutting down VM !!");
 
-				// sleep till the instance is in terminated state
-				try {
-					Thread.sleep(120000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                // detach volume and create a snapshot before 
+                // shutting down the VM
+                user1.shutdownVM();
 
-				// keep checking time, bring it up again at 8:00 am
-				String timeNow = now();
-				do {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					timeNow = now();
-				} while (!timeNow.contains(RESTART_TIME));
+                // sleep till the instance is in terminated state
+                try {
+                    Thread.sleep(120000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
 
-				// reload the VM in the morning
-				user1.reloadVM();
+                // keep checking time, bring it up again at 8:00 am
+                String timeNow = now();
+                do {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    timeNow = now();
+                } while (!timeNow.contains(RESTART_TIME));
 
-			} // end of if (!state)
+                // reload the VM in the morning
+                user1.reloadVM();
 
-		} // end of while (true)
+            } // end of if (!state)
 
-	} // end of run
+        } // end of while (true)
+
+    } // end of run
 
 } // end of class
--- SecurityCloud/src/coms6998/security/SecuredCloudManager.java
+++ SecurityCloud/src/coms6998/security/SecuredCloudManager.java
@@ -13,41 +13,41 @@ import coms6998.security.FileObject.FilePermission;
  */
 public class SecuredCloudManager {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		final String BUCKET_NAME = "edu.columbia.cloud.test";
-		
-		String username = "user";
-		String password = "passwordpassword";
-		
-		// create a single bucket for all the users		
-		S3 s3 = S3.getInstance();
-		S3Bucket bucket = s3.createBucket("jla2164");
-		
-		// create a user
-		User user = User.getInstance(username, password);
-		
-		// create a file object and set permissions on it
-		FileObject file = new FileObject("TestFile", "this is sample file");
-		file.setPermission(FilePermission.Private);
-		
-		try {
-			user.uploadFile(file);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			user.downloadFile("TestFile");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        final String BUCKET_NAME = "edu.columbia.cloud.test";
+
+        String username = "user";
+        String password = "passwordpassword";
+
+        // create a single bucket for all the users		
+        S3 s3 = S3.getInstance();
+        S3Bucket bucket = s3.createBucket("jla2164");
+
+        // create a user
+        User user = User.getInstance(username, password);
+
+        // create a file object and set permissions on it
+        FileObject file = new FileObject("TestFile", "this is sample file");
+        file.setPermission(FilePermission.Private);
+
+        try {
+            user.uploadFile(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            user.downloadFile("TestFile");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 
 }
